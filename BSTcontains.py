2d884552ca54eab044de3e9f3b477bc7f0ddda97
nivu235 <114389288+nivu235@users.noreply.github.com>
2024-06-11 12:54:03 +0530

    Add files via upload

---
+++ BSTcontains.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.left=None
+        self.right=None
+class Bst:
+    def __init__(self):
+        self.root=None
+    def insert(self,value):
+        new_node=Node(value)
+        if self.root is None:
+            self.root=new_node
+            return True
+        temp=self.root
+        while(True):
+            if new_node.value==temp.value:
+                return False
+            if new_node.value<temp.value:
+                if temp.left is None:
+                    temp.left=new_node
+                    return True
+                temp=temp.left
+            else:
+                if temp.right is None:
+                    temp.right=new_node
+                    return True
+                temp=temp.right
+    def contains(self,value):
+        temp=self.root
+        while temp is not None:
+            if value<temp.value:
+                temp=temp.left
+            elif value>temp.value:
+                temp=temp.right
+            else:
+                return True
+        return False            
+a=Bst()
+a.insert(2)
+a.insert(1)
+a.insert(3)
+a.insert(4)
+print(a.contains(4))
+print(a.contains(5))
+print(a.root.value)
+print(a.root.left.value)
+print(a.root.right.value)
